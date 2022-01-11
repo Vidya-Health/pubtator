0b0eadc7895d57412708b9e0d00353bf48f77845
= <=>
2022-01-11 12:02:26 +0000

    Bugfix utils nonimportable

--- pubtator/__init__.py
+++ pubtator/__init__.py
@@ -1,5 +1,6 @@
 from .parse import AnnotatedPubtatorAbstract, parse_pubtator_request_string
 from .request import request_annotations
+from . import pubmed
 
 __all__ = [
     "request_annotations",
