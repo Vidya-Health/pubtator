8a3daaecdcb90946c16d4345cdcae2c9469d7ccd
= <=>
2022-01-21 10:02:55 +0000

    New pubmed to pmid method

--- pubtator/pubmed.py
+++ pubtator/pubmed.py
@@ -1,4 +1,6 @@
 from Bio import Entrez
+import requests
+import json
 
 
 def get_pubmed_ids_in_daterange(email: str, start_date: int, end_date: int = 3000) -> list[int]:
@@ -30,24 +32,53 @@ def get_pubmed_ids_in_daterange(email: str, start_date: int, end_date: int = 300
     return pmids
 
 
-def pubmed_to_pmc(email: str, pmids: list[int]) -> list[str]:
-    """Converts pmids to pmcids."""
-    Entrez.email = email
+# def pubmed_to_pmc(email: str, pmids: list[int]) -> list[str]:
+#     """Converts pmids to pmcids."""
+#     Entrez.email = email
+#
+#     # Main function
+#     handle = Entrez.elink(dbfrom="pubmed", db="pmc", linkname="pubmed_pmc", id=pmids)
+#
+#     # Read results
+#     results = Entrez.read(handle)
+#
+#     # Open out the PMC ids
+#     pmcids = []
+#     for result in results:
+#         linkset = result['LinkSetDb']
+#         if len(linkset) > 0:
+#             pmcids.append(linkset[0]['Link'][0]['Id'])
+#         else:
+#             pmcids.append(None)
+#
+#     # Add the 'PMC' prefix to the PMC ids
+#     return [f"PMC{pmcid}" if pmcid is not None else None for pmcid in pmcids]
 
-    # Main function
-    handle = Entrez.elink(dbfrom="pubmed", db="pmc", linkname="pubmed_pmc", id=pmids)
 
-    # Read results
-    results = Entrez.read(handle)
+def pubmed_to_pmc(pmids: list[int]) -> dict[str, str]:
+    """Converts pmids to pmcids requesting from the url.
 
-    # Open out the PMC ids
-    pmcids = []
-    for result in results:
-        linkset = result['LinkSetDb']
-        if len(linkset) > 0:
-            pmcids.append(linkset[0]['Link'][0]['Id'])
-        else:
-            pmcids.append(None)
+    Args:
+        pmids: A list of pmids.
 
-    # Add the 'PMC' prefix to the PMC ids
-    return [f"PMC{pmcid}" if pmcid is not None else None for pmcid in pmcids]
+    Returns:
+        A dict that maps pmid to pmcid, if no pmcid pmid maps to None.
+    """
+    pmid_string = ','.join(str(pmid) for pmid in pmids)
+    url = f"""
+        https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids=
+        {pmid_string}&format=json
+    """.replace(' ', '').replace('\n', '')
+
+    # Request
+    response = requests.get(url)
+
+    # Handle
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        records = data['records']
+        mapping = {r['pmid']: r.get('pmcid', None) for r in records}
+    else:
+        raise Exception("Error: {response.status_code}")
+
+    return mapping
